29c4feb1fd61f1c9641fb2c3639a644ff5b5364c
Gerry Pantaleon <Gerry@192.168.1.8>
2016-07-02 00:46:03 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+/**
+ * Created by Gerry on 02/07/2016.
+ */
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+app.get('/',function (req,res) {
+    res.sendFile(__dirname+'/index.html')
+})
+io.on('connection',function (socket) {
+    console.log('one user connected : '+socket.id);
+})
+http.listen(3000, function () {
+    console.log('server listening on port 3000');
+})
